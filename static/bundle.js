a8db45f309b94561ff8440c6a483c77e1d3ccaea
mkdai <michaelkdai@gmail.com>
2017-08-14 14:43:40 -0700

    redirect ready for deploy v3.1

--- static/bundle.js
+++ static/bundle.js
@@ -36924,8 +36924,8 @@ Object.defineProperty(exports, "__esModule", {
 exports.default = {
     CLIENT_ID: 'Bt93UxMPclylp6P1iwcOY6ofPQsNeSZo',
     DOMAIN: 'michaelkdai.auth0.com',
-    REDIRECT_URI: 'http://localhost:3000/',
-    //REDIRECT_URI: 'https://boomboomcats.herokuapp.com/',
+    //REDIRECT_URI: 'http://localhost:3000/',
+    REDIRECT_URI: 'https://boomboomcats.herokuapp.com/',
     AUDIENCE: 'https://michaelkdai.auth0.com/userinfo'
 };
 
